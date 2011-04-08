e4c75d4a325d0258387f44e502fedfde74d5ef8c
Sebastian Rahlf <basti@redtoad.de>
2011-04-08 11:45:58 +0200

    Completed package information.

--- setup.py
+++ setup.py
@@ -1,42 +1,37 @@
-import os.path
 from setuptools import setup
 
 import pytest_localserver
 
 def read(fname):
     # makes sure that setup can be executed from a different location
+    import os.path
     _here = os.path.abspath(os.path.dirname(__file__))
     return open(os.path.join(_here, fname)).read()
 
 setup(
     name='pytest-localserver',
     version = pytest_localserver.VERSION,
-    description = 'py.test plugin to test server connections locally.',
-    long_description=read('README'),
-    license = 'mit',
-
     author = 'Sebastian Rahlf',
     author_email = 'basti AT redtoad DOT de',
+    license = 'MIT License',
+    description = 'py.test plugin to test server connections locally.',
+    long_description=read('README'),
+    url='http://bitbucket.org/basti/pytest-localserver/',
+    download_url='http://bitbucket.org/basti/pytest-localserver/downloads/',
 
     packages = ['pytest_localserver'],
     install_requires = ['pytest>=2.0.0'],
-
-    # the following makes the plugin available to py.test
-    entry_points = {
-        'pytest11': [
-            'localserver = pytest_localserver.plugin',
-        ]
-    },
-
+    entry_points = {'pytest11': ['localserver = pytest_localserver.plugin']},
     zip_safe=False,
 
+    keywords='py.test pytest server localhost http smtp',
     classifiers = [
         'Operating System :: OS Independent',
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Testing'
     ]
 )
