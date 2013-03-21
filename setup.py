e80f7a017cafd0fbac13d883eacdf716d36a8f8c
Sebastian Rahlf <basti@redtoad.de>
2013-03-21 23:09:45 +0100

    Version bumped.

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
+import sys
 
-import pytest_localserver
+
+VERSION = '0.1.5'
+
 
 def read(fname):
     # makes sure that setup can be executed from a different location
@@ -8,29 +11,36 @@ def read(fname):
     _here = os.path.abspath(os.path.dirname(__file__))
     return open(os.path.join(_here, fname)).read()
 
+# make sure that versions match before uploading anything to the cheeseshop
+if 'upload' in sys.argv or 'register' in sys.argv:
+    import pytest_localserver
+    assert pytest_localserver.VERSION == VERSION
+
 setup(
     name='pytest-localserver',
-    version = pytest_localserver.VERSION,
-    author = 'Sebastian Rahlf',
-    author_email = 'basti AT redtoad DOT de',
-    license = 'MIT License',
-    description = 'py.test plugin to test server connections locally.',
+    version=VERSION,
+    author='Sebastian Rahlf',
+    author_email='basti AT redtoad DOT de',
+    license='MIT License',
+    description='py.test plugin to test server connections locally.',
     long_description=read('README'),
     url='http://bitbucket.org/basti/pytest-localserver/',
     download_url='http://bitbucket.org/basti/pytest-localserver/downloads/',
 
-    packages = ['pytest_localserver'],
-    install_requires = [
-        'pytest>=2.0.0', 
+    packages=['pytest_localserver'],
+    install_requires=[
+        'pytest>=2.0.0',
         'pyOpenSSL'
     ],
-    entry_points = {'pytest11': ['localserver = pytest_localserver.plugin']},
+    entry_points={
+        'pytest11': ['localserver = pytest_localserver.plugin']
+    },
 
     zip_safe=False,
     include_package_data=True,
 
     keywords='py.test pytest server localhost http smtp',
-    classifiers = [
+    classifiers=[
         'Operating System :: OS Independent',
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
