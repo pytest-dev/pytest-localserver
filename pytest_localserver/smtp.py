c08ea28af7631ebb70aaeb01eac4df31f0171c48
David Zaslavsky <diazona@ellipsix.net>
2023-06-25 19:51:09 -0700

    Remove a structure used for compatbility with Python 2.4
    Python 2.4 is no longer anywhere close to supported so we can put
    `except` and `finally` in the same `try` block now.

--- pytest_localserver/smtp.py
+++ pytest_localserver/smtp.py
@@ -166,18 +166,13 @@ def main():
     print("Type <Ctrl-C> to stop")
 
     try:
-
-        try:
-            while True:
-                time.sleep(1)
-        finally:
-            print("\rstopping...")
-            server.stop()
-
+        while True:
+            time.sleep(1)
     except KeyboardInterrupt:
-        # support for Python 2.4 dictates that try ... finally is not used
-        # together with any except statements
         pass
+    finally:
+        print("\rstopping...")
+        server.stop()
 
 
 if __name__ == "__main__":  # pragma: no cover
